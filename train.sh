5bd327021a7b314405c03f5144beac41a0b418a8
Jiahao9 <simjiahao9@gmail.com>
2026-02-05 15:38:41 +0900

    [ADD] gs training only

--- train.sh
+++ train.sh
@@ -3,6 +3,16 @@ DATASET_NAME=$2
 INDEX=$3
 TOPK=4
 
+## Train Gaussian Splatting only
+## set self.include_feature = False in arguments/__init__.py
+# python train.py \
+#     -s $DATASET_ROOT_PATH/$DATASET_NAME \
+#     -m output/${DATASET_NAME}_${INDEX} \
+#     --vq_layer_num 1 \
+#     --codebook_size 64 \
+#     --cos_loss \
+#     --topk $TOPK
+
 for level in 1 2 3
 do
     python train.py \
